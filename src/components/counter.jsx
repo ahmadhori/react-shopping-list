275585b65e207f3b2d7c0c337c184443638560c8
Ahmad Houri <Ahmad.hori@gmail.com>
2020-01-02 22:47:25 +0100

    Handling increment btn click event Set state instead od editing state directly

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,6 +17,7 @@ class Counter extends Component {
         <button
           style={{ fontSize: 14, fontWeight: "bold" }}
           className="btn btn-primary m-2"
+          onClick={this.incrementBtnClick}
         >
           Increment
         </button>
@@ -24,12 +25,19 @@ class Counter extends Component {
     );
   }
 
+  incrementBtnClick = () => {
+    console.log("Hello from increment btn", this);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   formatCount() {
-    return this.state.count == 0 ? "Zero" : this.state.count;
+    return this.state.count === 0 ? "Zero" : this.state.count;
   }
 
   counterClasses() {
-    return this.state.count == 0 ? "btn btn-danger m-2" : "btn btn-warning m-2";
+    return this.state.count === 0
+      ? "btn btn-danger m-2"
+      : "btn btn-warning m-2";
   }
 }
 
