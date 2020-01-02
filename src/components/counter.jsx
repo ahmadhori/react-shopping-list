cb0ab13b014c9ed3f8f47341912803b6b64f04a2
Ahmad Houri <Ahmad.hori@gmail.com>
2020-01-02 06:16:32 +0100

    Added btn and a styled number to counter compoonent

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0
+  };
+
+  styles = {
+    fontSize: 10
+  };
+
   render() {
-    return <h1>Hello Counter</h1>;
+    return (
+      <div>
+        <span style={this.styles} className="badge badge-secondary m-2">
+          {this.formatCount()}
+        </span>
+        <button
+          style={{ fontSize: 14, fontWeight: "bold" }}
+          className="btn btn-primary m-2"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    return this.state.count == 0 ? "Zero" : this.state.count;
   }
 }
 
