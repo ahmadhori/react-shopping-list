0bef957fe44efe0ac84beada102df52272e050f5
Ahmad Houri <Ahmad.hori@gmail.com>
2020-01-03 06:58:55 +0100

    Adding Delete Button Adding Reset Button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,14 +11,37 @@ class Counters extends Component {
     ]
   };
 
-  constructor(props) {
-    super(props);
+  handleDelete(id) {
+    console.log("Delete handeled ", id);
+    const x = this.state.counters.filter(x => x.id !== id);
+    this.setState({ counters: x });
   }
+
+  handleReset = () => {
+    const xx = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    console.log(xx);
+    this.setState({ counters: xx });
+  };
+
   render() {
     return (
       <div>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} id={counter.id} count={counter.value}>
+          <Counter
+            key={counter.id}
+            id={counter.id}
+            count={counter.value}
+            onClick={() => this.handleDelete(counter.id)}
+          >
             <h2>Counter {counter.id}</h2>
           </Counter>
         ))}
